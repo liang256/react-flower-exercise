5b98ac97314f1539561104e9d0f0eeb0fe12e24a
LIANGYU <n795113@gmail.com>
2022-04-08 15:55:00 +0800

    fix: error when init

--- src/StoryModule.js
+++ src/StoryModule.js
@@ -7,6 +7,7 @@ export function getAllAnalysis() {
     }
     try {
         analysis = JSON.parse(analysis)
+        analysis = (analysis === null) ? [] : analysis
     } catch (e) {
         analysis = []
     }
